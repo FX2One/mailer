c8eda07fc5767a7050107e1c7341aa45af06b0d7
Michał Skrzypczak <mskrzypczak2@swps.edu.pl>
2021-04-26 13:19:39 +0200

    configured mimebase and attachements

--- mailer.py
+++ mailer.py
@@ -18,10 +18,27 @@ password = getpass.getpass(prompt='Password: ')
 subject = input('Type in email subject: ')
 
 # receivers
-receiver_email = input('type in receiver: ') #will be changed to read from file
+receiver_email = input('type in receiver: ') # will be changed to read from file
 
 # email message
-e_msg = input('Your Message here: ') #will be changed to read from file
+e_msg = input('Your Message here: ') # will be changed to read from file
+
+# will be changed for pop-up file dialog box
+file_location = 'C:\\Users\\mskrzy16\\Desktop\\' # on default Desktop, type in
+file_name = input('Type in file name with extension: ')
+file_fullname = file_location + file_name
+
+# open attachement
+attachment = open(file_fullname, 'rb')
+
+# MimeBase instance
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read()) # read the attachment
+encoders.encode_base64(part) # encode to base64
+part.add_header('Content-Disposition', f'attachment; filename= {file_fullname}')
+
+
+
 
 #Mime multipart headers
 message = MIMEMultipart()
@@ -34,11 +51,6 @@ message['Subject'] = subject
 # email body
 message.attach(MIMEText(e_msg, "plain"))
 
-
-
-
-
-
 # secure SSL certificate
 context = ssl.create_default_context()
 
@@ -46,6 +58,7 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message.as_string())
+    server.quit()
 
 
 

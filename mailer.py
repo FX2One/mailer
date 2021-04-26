57a168410fae3f966bf57a7da67a11798c8ba116
FX2One <dfoxik@gmail.com>
2021-04-26 15:58:57 +0200

    working mailer, minor fixes to be applied

--- mailer.py
+++ mailer.py
@@ -5,40 +5,39 @@ from email import encoders
 from email.mime.base import MIMEBase
 import getpass
 
+
 smtp_server = "smtp.gmail.com"
 port = 465  # starttls TransportLayerSecurity
 
 # type in your address
-sender_email = input('Type in your email address: ')
+sender_email = '' # to be filled later
 
 # unix like password typing
 password = getpass.getpass(prompt='Password: ')
 
 # email subject
-subject = input('Type in email subject: ')
+subject = 'Test email for my stuff'
 
 # receivers
-receiver_email = input('type in receiver: ') # will be changed to read from file
+receiver_email = '' # will be changed to read from file
 
 # email message
-e_msg = input('Your Message here: ') # will be changed to read from file
+e_msg = 'thats my test message ' # will be changed to read from file
 
 # will be changed for pop-up file dialog box
-file_location = 'C:\\Users\\mskrzy16\\Desktop\\' # on default Desktop, type in
-file_name = input('Type in file name with extension: ')
-file_fullname = file_location + file_name
+#fix %username% position
+file_location = 'C:\\Users\\%username%\\Downloads\\readme.txt'  # so far takes file from Downloads ,doesn't work quite yet
 
-# open attachement
-attachment = open(file_fullname, 'rb')
+# function that gets the last element of a path string to send user only file name not entire path directory
+def get_file_name(our_file):
+    x = []
+    s = our_file.replace('\\',' ')
+    for word in s.split():
+        x.append(word)
+    return x[-1]
 
-# MimeBase instance
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read()) # read the attachment
-encoders.encode_base64(part) # encode to base64
-part.add_header('Content-Disposition', f'attachment; filename= {file_fullname}')
-
-
-
+# stores file_location name of a file we want to attach
+f1 = get_file_name(file_location)
 
 #Mime multipart headers
 message = MIMEMultipart()
@@ -48,8 +47,15 @@ message['Bbc'] = receiver_email #blind carbon copy
 message['Subject'] = subject
 
 
-# email body
-message.attach(MIMEText(e_msg, "plain"))
+# open attachement
+attachment = open(file_location, 'rb')
+
+# MimeBase instance
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read()) # read the attachment
+encoders.encode_base64(part) # encode to base64
+part.add_header('Content-Disposition', f'attachment; filename= {f1}')
+message.attach(part) # add attachment to message
 
 # secure SSL certificate
 context = ssl.create_default_context()
